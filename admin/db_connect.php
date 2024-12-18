8904a02376bb2e5bed8099f6d49b350918dcab88
yanyanbadiable <yanyanbadiable@gmail.com>
2024-12-18 18:01:06 +0800

    final

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -2,4 +2,4 @@
 
 // $conn= new mysqli('sql100.infinityfree.com','if0_37775276','5ce6rvKoxGtMlxx','if0_37775276_scheduling_db')or die("Could not connect to mysql".mysqli_error($con));
 
-$conn = new mysqli('localhost', 'root', '', 'scheduling_db') or die("Could not connect to mysql" . mysqli_error($con));
+$conn = new mysqli('srv1650.hstgr.io', 'u532201941_adrums', 'Adrumsdb123', 'u532201941_scheduling_db') or die("Could not connect to mysql" . mysqli_error($con));
